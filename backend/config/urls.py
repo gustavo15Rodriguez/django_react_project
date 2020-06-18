d566c1de1f818e9983b1decc0f1af7216b76b6e3
Gustavo A. Rodriguez <garodriguez263@gmail.com>
2020-06-17 19:35:59 -0500

    Api rest created.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
+from backend.apps.customers import views
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -16,6 +17,8 @@ urlpatterns = [
     path("users/", include("backend.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("api/customers/", views.customer_list),
+    path("api/customers/<int:pk>", views.customers_detail)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
